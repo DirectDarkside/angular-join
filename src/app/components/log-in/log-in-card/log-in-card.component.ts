1e11ed77469dac79cf1a39f23296b54381c81967
DirectDarkside <109902468+DirectDarkside@users.noreply.github.com>
2025-02-25 21:39:11 +0100

    added log-in-card component

---
+++ src/app/components/log-in/log-in-card/log-in-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-card',
+  imports: [],
+  templateUrl: './log-in-card.component.html',
+  styleUrl: './log-in-card.component.scss'
+})
+export class LogInCardComponent {
+
+}
